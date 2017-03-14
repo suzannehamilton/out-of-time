427357c15e78f009bec4e19a87507d791c65b23a
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2017-03-14 18:46:39 +0000

    Create admin dashboard with administrate gem
    Add gem and generate dashboard for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :events
+
+    root to: "events#index"
+  end
+
   root to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
